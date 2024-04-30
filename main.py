970723c3b0c9513c7e30f79c007def7459114585
areeb1501 <64038736+areeb1501@users.noreply.github.com>
2024-04-30 10:03:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allowed request methods
     allow_headers=["*"],  # Allowed request headers
